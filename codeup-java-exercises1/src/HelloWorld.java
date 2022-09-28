21ab82d8289b1725cfb7abceca389be815e5b58a
Nicholas Barrera <nickbarerra722@gmail.com>
2022-09-28 08:58:11 -0500

    update

---
+++ codeup-java-exercises1/src/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    int id = 46;
+    boolean trueOrFalse = true;
+    char character = 'g';
+    double price = 5.99;
+
+
+    public static void main(String[] args) {
+
+        int age;
+        age =21;
+
+
+
+
+        System.out.println("Hello, World!");
+
+        System.out.println(age);
+    }
+
+}
